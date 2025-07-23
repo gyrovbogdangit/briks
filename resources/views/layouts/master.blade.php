f13e12b777e025b2d795ac6c67e45b53df775f87
Bogdan Gyrov <gyrovbogdan@mail.ru>
2025-07-23 13:19:57 +0300

    Добавил Яндекс Метрику

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,6 +12,37 @@
         rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet" />
 
+    <!-- Yandex.Metrika counter -->
+    <meta name="yandex-verification" content="70e92b50ce025327"  />
+    <script type="text/javascript">
+        (function(m, e, t, r, i, k, a) {
+            m[i] = m[i] || function() {
+                (m[i].a = m[i].a || []).push(arguments)
+            };
+            m[i].l = 1 * new Date();
+            for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) {
+                    return;
+                }
+            }
+            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(
+                k, a)
+        })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=103434813', 'ym');
+
+        ym(103434813, 'init', {
+            ssr: true,
+            webvisor: true,
+            clickmap: true,
+            ecommerce: "dataLayer",
+            accurateTrackBounce: true,
+            trackLinks: true
+        });
+    </script>
+    <noscript>
+        <div><img src="https://mc.yandex.ru/watch/103434813" style="position:absolute; left:-9999px;" alt="" />
+        </div>
+    </noscript>
+    <!-- /Yandex.Metrika counter -->
     @yield('css')
     @livewireStyles
 </head>
@@ -34,7 +65,7 @@
     @yield('js')
     @livewireScripts
     <script>
-        document.addEventListener('DOMContentLoaded', function () {
+        document.addEventListener('DOMContentLoaded', function() {
             if (!localStorage.getItem('cookieAccepted')) {
                 const cookieBanner = document.createElement('div');
                 cookieBanner.innerHTML = `
@@ -44,7 +75,7 @@
                     </div>
                 `;
                 document.body.appendChild(cookieBanner);
-                document.getElementById('cookie-accept-btn').onclick = function () {
+                document.getElementById('cookie-accept-btn').onclick = function() {
                     localStorage.setItem('cookieAccepted', '1');
                     document.getElementById('cookie-banner').remove();
                 };
