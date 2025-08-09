80029c68d01fb66703fe3ee7fe3023bfbb6dd2e7
Bogdan Gyrov <gyrovbogdan@mail.ru>
2025-08-09 14:56:18 +0300

    Отключил бэкапы

--- routes/console.php
+++ routes/console.php
@@ -3,5 +3,3 @@
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('queue:work --stop-when-empty --tries=3')->everyMinute();
-Schedule::command('backup:clean')->daily()->at('03:00');
-Schedule::command('backup:run')->daily()->at('03:30');
